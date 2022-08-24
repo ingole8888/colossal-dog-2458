861547f9aaa642f8616996e089e8875e0d7aebd1
ingole8888 <ingole8888@gmail.com>
2022-08-24 15:44:21 +0530

    change

--- src/Routes/CartPage.jsx
+++
@@ -1,8 +0,0 @@
-function CartPage(){
-    return (
-        <>
-        <h1>Cart Page</h1>
-        </>
-    )
-}
-export default CartPage
--- src/Routes/ProductPage.jsx
+++
@@ -1,8 +0,0 @@
-function ProductPage(){
-    return (
-        <>
-        <h1>Product Page</h1>
-        </>
-    )
-}
-export default ProductPage
