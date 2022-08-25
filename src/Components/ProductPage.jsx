7aa8beab62e12e6ae5cd3d4b886fb77334d18187
ingole8888 <ingole8888@gmail.com>
2022-08-25 13:03:26 +0530

    routing completed

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 function ProductPage(){
     return (
         <>
+        <Navbar/>
         <h1>Product Page</h1>
+        <Footer/>
         </>
     )
 }
