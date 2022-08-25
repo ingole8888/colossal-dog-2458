474e27b5a2a70802867a9126b7cff7f8774da8b9
ingole8888 <ingole8888@gmail.com>
2022-08-25 15:27:30 +0530

    product page styling completed

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -1,10 +1,35 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./ProductPage.css"
+
 function ProductPage(){
+    const [data, setData]=useState([]);
+
+    useEffect(()=>{
+        axios("https://fake-restful-api1.herokuapp.com/Allproducts")
+        .then((res)=> setData(res.data))
+    });
+    console.log(data)
     return (
         <>
         <Navbar/>
-        <h1>Product Page</h1>
+        <div className="productdata">
+        {data.map((items) => {
+           return(
+            <div className="subitems">
+            <img src={items.image} alt=""/>
+            <h4>{items.name}</h4>
+            <h2>{items.price}</h2>
+            <p>{items.details}</p>
+            <button>Add to Cart</button>
+            </div>
+            
+           )
+        })}
+        
+        </div>
         <Footer/>
         </>
     )
